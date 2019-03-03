0830c67e489184e009811cee2902d578b99edc28
rupak <rupak4@yandex.ru>
2019-03-03 18:01:26 +0300

    Emmiter

--- public/index.php
+++ public/index.php
@@ -3,9 +3,10 @@
 error_reporting(-1);
 header('Content-Type: text/html; charset=utf-8');
 
-use Framework\Http\ResponseSender;
+
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\ServerRequestFactory;
+use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
@@ -17,5 +18,5 @@ $name = $request->getQueryParams()['name'] ?? 'Гость';
 $response =(new HtmlResponse('Hello, ' . $name . '!'))
     ->withHeader('X-Developer', 'Test');
 
-$emitter = new ResponseSender();
+$emitter = new SapiEmitter();
 $emitter->send($response);
